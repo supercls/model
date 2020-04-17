adbcf1913dd57f3b50c645c6583f3215e11ab2d3
supercls <992837162@qq.com>
2020-04-17 23:44:25 +0800

    1

---
+++ src/pages/Header/Header.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+const ModelHeader = () =>{
+    return(
+        <header className="Eduwrapper">
+            <ul>
+                <li>在线教育</li>
+                <li>我的课程</li>
+                <li>个人中心</li>
+            </ul>
+        </header>
+    )
+}
+export default ModelHeader
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './Home.module.css'
+import ModelHeader from '../Header/Header'
+const ModelHome = () =>{
+    return(
+        <div className={style.Eduwrapper}>
+            <span>1</span>
+            <ModelHeader></ModelHeader>
+        </div>
+    )
+}
+export default ModelHome
